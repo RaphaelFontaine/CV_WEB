fa634c9f12777de946829b0d0cfa0ff2759ea26e
rfontaine24 <raphael.fontaine62@outlook.fr>
2023-01-27 22:37:46 +0100

    components folder + beginning header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import Head from "next/head";
+import { HeaderMenuColored } from "../../components/header";
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world! 
-    </h1>
+    <div>
+      <Head>
+        <title>Raphael FONTAINE</title>
+        <meta name="description" content="CV Raphael Fontaine Data Engineer Developer" />
+        <link rel="icon" href="/letter-R.png" />
+      </Head>
+      <main>
+        fjgotgjrt
+      </main>
+    </div>
   )
 }
