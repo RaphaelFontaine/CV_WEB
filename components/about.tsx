45b46d4ac5835ee32dd7c603e6629bb88ee5a15b
rfontaine24 <raphael.fontaine62@outlook.fr>
2023-01-31 17:57:14 +0100

    about section

--- components/about.tsx
+++ components/about.tsx
@@ -1,11 +1,10 @@
 import BackgroundAnimation from "@/utils/backgroundAnimation";
-import { Button } from "@mantine/core";
 
 export default function About(){
     return(
-        <div className="relative bg-primary min-h-screen" id='about'>
+        <div className="flex bg-primary min-h-screen" id='about'>
 
-            <div className="">
+            <div>
                 <BackgroundAnimation/>
             </div>
             <div className="flex flex-col justify-center h-screen pb-20 max-w-5xl mx-auto z-50 px-6">
@@ -18,17 +17,18 @@ export default function About(){
                 <div className="text-third">
                     I'm a french Data Engineer
                 </div>
-                <div className="flex flex-row justify-center items-center space-x-8 pt-20">
-                    <a href='#contact' className="button-contact"> 
+                <div className="flex flex-row pt-20 space-x-10">
+                    <a href='#contact' className="button-contact transition-all duration-500"> 
                         Contact Me
                     </a>
-                    <Button className="bg-fourth">
-                        Contact Me
-                    </Button>
-                    <Button className="bg-fourth">
-                        View my work
-                    </Button>
-
+                    <a href='#formations' className="button-contact group transition-all duration-500"> 
+                        <span className="flex flex-row">
+                            View my work
+                            <svg width="20px" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 flex justify-center ml-4 items-center transition-all duration-500 group-hover:rotate-90" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
+                            </svg>
+                        </span>
+                    </a>
                 </div>
             </div>
             
