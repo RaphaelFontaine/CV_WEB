b7d6008d231ca677e70b8e9950522ff035b1dc28
rfontaine24 <raphael.fontaine62@outlook.fr>
2023-01-31 13:41:34 +0100

    formations function

--- components/formations.tsx
+++ components/formations.tsx
@@ -1,7 +1,41 @@
-export default function Formations(){
+import { FORMATIONS } from "../utils/formations"
+
+interface FormationsProps {
+    title: string,
+    date: string,
+    text: string
+}
+
+export function Formation(props: FormationsProps){
+    let {title, date, text} = props
     return(
         <div id='formations'>
-            Formations Section <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <div>
+                <div className='bg-third'>
+                    {title}
+                </div>
+                <div className="bg-secondary">
+                    {date}
+                </div>
+                <div className="bg-primary">
+                    {text}
+                </div>
+            </div>
         </div>
+    )
+}
+
+export default function Formations(){
+    return (
+        <>
+            {FORMATIONS.map((formation : any, index : any) => 
+                <Formation 
+                    key={index}
+                    title={formation.title}
+                    date={formation.date}
+                    text={formation.text}
+                />
+            )}
+        </>
     )
 } 
---
+++ utils/formations.ts
@@ -0,0 +1,17 @@
+export const FORMATIONS = [
+    {
+        title: "Computer Science Engineer ENSEIRB-MATMECA",
+        date: "2018-2021 / Bordeaux",
+        text: "Computer science specification",
+    },
+    {
+        title: "Prepa SALIEGE",
+        date: "2016-2018 / Toulouse",
+        text: "PC*",
+    },
+    {
+        title: "Lycée EMILIE DE RODAT",
+        date: "2013-2016 / Toulouse",
+        text: "Bac S",
+    }
+]
