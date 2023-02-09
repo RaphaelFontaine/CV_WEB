43ce416703189610e5722cf32f77ff1930d76299
rfontaine24 <raphael.fontaine62@outlook.fr>
2023-02-09 14:26:07 +0100

    animations about

--- components/about.tsx
+++ components/about.tsx
@@ -12,7 +12,7 @@ function FadeIn({children} : any) {
           style={{
             transform: isInView ? "none" : "translateY(5vw)",
             opacity: isInView ? 1 : 0,
-            transition: "all 2s cubic-bezier(0.17, 0.55, 0.55, 1)"
+            transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1)"
           }}
         >
           {children}
@@ -31,7 +31,7 @@ function FadeIn({children} : any) {
           style={{
             transform: isInView ? "none" : "translateX(-5vw)",
             opacity: isInView ? 1 : 0,
-            transition: "all 1s cubic-bezier(0.17, 0.55, 0.55, 1) 2s"
+            transition: "all 1s cubic-bezier(0.17, 0.55, 0.55, 1) 1s"
           }}
         >
           {children}
@@ -50,7 +50,7 @@ function FadeIn({children} : any) {
           style={{
             transform: isInView ? "none" : "translateX(5vw)",
             opacity: isInView ? 1 : 0,
-            transition: "all 1s cubic-bezier(0.17, 0.55, 0.55, 1) 2s"
+            transition: "all 1s cubic-bezier(0.17, 0.55, 0.55, 1) 1s"
           }}
         >
           {children}
@@ -77,23 +77,21 @@ export default function About(){
                     <h2 className="text-third">
                         I'm a french Data Engineer
                     </h2>
+                    <FadeLeft>
                     <div className="flex flex-row pt-20 space-x-10">
-                        <FadeLeft>
-                            <a href="#contact" className="button-contact transition-all duration-500 scroll-smooth"> 
-                                Contact Me
-                            </a>
-                        </FadeLeft>
-                        <FadeRight>
-                            <a href='#experiences' className="button-contact group transition-all duration-500 scroll-smooth"> 
-                                <span className="flex flex-row">
-                                    View my work
-                                    <svg width="20px" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 flex justify-center ml-4 items-center transition-all duration-500 group-hover:rotate-90" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
-                                    </svg>
-                                </span>
-                            </a>
-                        </FadeRight>
+                        <a href="#contact" className="button-contact transition-all duration-500 scroll-smooth"> 
+                            Contact Me
+                        </a>
+                        <a href='#experiences' className="button-contact group transition-all duration-500 scroll-smooth"> 
+                            <span className="flex flex-row">
+                                View my work
+                                <svg width="20px" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 flex justify-center ml-4 items-center transition-all duration-500 group-hover:rotate-90" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
+                                </svg>
+                            </span>
+                        </a>
                     </div>
+                    </FadeLeft>
                 </div>
             </div>
         </FadeIn>
