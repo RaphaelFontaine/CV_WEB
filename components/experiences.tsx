7d1fccbc8370b214a1f162e7e7824635cfb66b12
rfontaine24 <raphael.fontaine62@outlook.fr>
2023-02-09 11:57:03 +0100

    organisation assets

--- components/experiences.tsx
+++ components/experiences.tsx
@@ -1,26 +1,10 @@
 export default function Experiences(){
     return(
         <div className="flex flex-wrap items-center justify-center w-full space-x-16 px-10 gap-x-10 gap-y-20 py-20">
-            <div className="box">
-                <span></span>
-                    <div className="content">
-                    <img src="/assets/tehtris.png" className="z-50">
+            <div className="flex justify-center items-center w-16 h-16 md:w-20 md:h-20 rounded-full md:p-5 p-3 bg-fifth hover:shadow-xl hover:shadow-zinc-900 transition-all duration-500 hover:-translate-y-2 hover:scale-105 overflow-hidden backdrop-blur-lg bg-gradient-to-br from-[#fff]/20 to-[#fff]/0 brightness-105">
+                <img src="/assets" className="z-50">
                 </img>
-                </div>
-            </div>
-            <div className="box">
-                <span></span>
-                    <div className="content">
-                    <img src="/assets/atos.png" className="z-50">
-                    </img>
-            </div>
-            </div>
-            <div className="box">
-                <span></span>
-                    <div className="content">
-                <h2>Hover me!</h2>
             </div>
         </div>
-    </div>
     )
 } 
