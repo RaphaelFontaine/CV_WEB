60b8c47c89f4c1ad6f230ea029faf5eaedd40672
RaphaelFontaine <raphael.fontaine62@outlook.fr>
2023-02-22 23:22:39 +0100

    animation skills not onView

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,7 +1,6 @@
-import { SKILLS } from "@/utils/skills"
 import * as React from "react";
-import { motion, useInView } from "framer-motion";
-import { useRef } from "react";
+import { motion } from "framer-motion";
+import { SKILLS } from "@/utils/skills"
 
 interface SkillProps {
     name: string,
@@ -9,63 +8,47 @@ interface SkillProps {
 }
 
 const skillVariants = {
-    hidden: { y: 20, opacity: 0 },
-    visible: {
-        y: 0,
-        opacity: 1
-    }
+    visible: { opacity: 1, y: 0 },
+    hidden: { opacity: 0, y: 50 }
   };
 
-export function Skill(props: SkillProps){
-    let {name, logo} = props
-
-    return(
-            <motion.div variants={skillVariants} initial="hidden" animate="visible">
-                <div className="flex flex-col justify">
-                    <motion.div className="flex justify-center items-center w-16 h-16 md:w-20 md:h-20 rounded-full md:p-5 p-3 bg-fifth hover:shadow-xl hover:shadow-zinc-900 transition-all duration-500 hover:-translate-y-2 hover:scale-105 overflow-hidden backdrop-blur-lg bg-gradient-to-br from-[#fff]/20 to-[#fff]/0 brightness-105">
-                        <img src={logo} className="z-50">
-                        </img>
-                    </motion.div>
-                    <motion.h1 className="text-center text-skills">
-                        {name}
-                    </motion.h1>
-                </div>
-            </motion.div>
-    )
-}
-
-export default function Skills(){
-    const ref = useRef(null);
-    const isInView = useInView(ref, { once: true });
-
+export function Skill(props: SkillProps) {
+    const { name, logo } = props;
+  
     return (
-        <div id="formations">
-            <section ref={ref}>
-                <motion.div
-                    className="flex flex-wrap justify-center gap-x-6 gap-y-12 max-w-lg px-2"
-                    variants={
-                        {
-                        visible: {
-                            transition: { staggerChildren: 0.1, delayChildren: 0.3 },
-                        },
-                        hidden: {},
-                        }
-                    }
-                    initial={isInView ? "visible" : "hidden"}
-                    animate={isInView ? "visible" : "hidden"}
-                >
-                    {SKILLS.map((formation: any, index: any) => (
-                    <motion.div
-                        key={index}
-                        variants={skillVariants}
-                        initial="hidden"
-                        animate={isInView ? "visible" : "hidden"}
-                    >
-                        <Skill name={formation.name} logo={formation.logo} />
-                    </motion.div>
-                    ))}
-                </motion.div>
-            </section>
+      <motion.div variants={skillVariants} initial="hidden" animate="visible">
+        <div className="flex flex-col justify">
+          <motion.div className="flex justify-center items-center w-16 h-16 md:w-20 md:h-20 rounded-full md:p-5 p-3 bg-fifth hover:shadow-xl hover:shadow-zinc-900 transition-all duration-500 hover:-translate-y-2 hover:scale-105 overflow-hidden backdrop-blur-lg bg-gradient-to-br from-[#fff]/20 to-[#fff]/0 brightness-105">
+            <img src={logo} className="z-50" />
+          </motion.div>
+          <motion.h1 className="text-center text-skills">{name}</motion.h1>
         </div>
-    )
-} 
+      </motion.div>
+    );
+  }
+  
+
+
+  export default function Skills() {
+    return (
+      <div id="formations">
+        <motion.div
+          className="flex flex-wrap justify-center gap-x-6 gap-y-12 max-w-lg px-2"
+          variants={{
+            visible: {
+              transition: { staggerChildren: 0.1, delayChildren: 0.3 }
+            },
+            hidden: {}
+          }}
+          initial="hidden"
+          animate="visible"
+        >
+          {SKILLS.map((formation: any, index: any) => (
+            <motion.div variants={skillVariants} key={index}>
+              <Skill name={formation.name} logo={formation.logo} />
+            </motion.div>
+          ))}
+        </motion.div>
+      </div>
+    );
+  }
